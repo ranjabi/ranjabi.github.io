0d5636617c7bc67d55dd402a7ad4b4ea97020cbc
ranjabi <fikriranjabi02@gmail.com>
2022-06-01 19:11:30 +0700

    add tailwind css and a little bit intro

---
+++ src/components/Hero.js
@@ -0,0 +1,13 @@
+const Hero = () => {
+  return (
+    <section id="home" className="pt-36">
+      <div className="container">
+        <div className="flex flex-wrap">
+          <div className="w-full "></div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
