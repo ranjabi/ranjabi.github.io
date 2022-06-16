82470553be33503d2a116ab8296dca2d867eeee8
ranjabi <fikriranjabi02@gmail.com>
2022-06-16 14:34:05 +0700

    add project image

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,20 +1,23 @@
 const Card = (props: any) => {
   return (
-      <div className="m-4 p-4 md:w-5/12 lg:w-3/12 drop-shadow-xl rounded-[6px] bg-primary  z-[1]">
-        <h1 className="mb-4 text-xl font-semibold">{props.title}</h1>
-        <img
-          src={props.img}
-          alt=""
-          className="rounded-[4px]"
-        />
-        <p className="mt-4 text-gray-600 font-medium">
-          {props.description}
-        </p>
-        <p className="mt-5 text-sm font-bold text-gray-700 tracking-widest">
-          {props.tech}
-        </p>
-        {/* <div className="bg-secondary top-[12px] left-[12px] rounded-[6px] ">&nbsp;</div> */}
-      </div>
+    <a
+      href={props.github}
+      className="relative m-4 p-4 md:w-5/12 lg:w-3/12 drop-shadow-xl rounded-[6px] bg-primary  z-[1]"
+    >
+      <h1 className="mb-4 text-xl font-semibold">{props.title}</h1>
+      <img
+        src={props.img}
+        alt=""
+        className="h-40 xl:h-52 object-cover w-full rounded-[4px]"
+      />
+      <p className="mt-4 mb-10 text-gray-600 font-medium">
+        {props.description}
+      </p>
+      <p className="absolute bottom-0 pb-4 text-sm font-bold text-gray-700 tracking-widest">
+        {props.tech}
+      </p>
+      {/* <div className="bg-secondary top-[12px] left-[12px] rounded-[6px] ">&nbsp;</div> */}
+    </a>
   );
 };
 
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,22 +1,25 @@
 import Card from './Card';
 import { Fragment } from 'react';
 
+const dnatesterimg = require('../assets/dnatester.png');
+const myemotionsimg = require('../assets/myemotions.png');
+
 const projectList = [
   {
+    title: 'My Emotions',
+    img: myemotionsimg,
+    desc: 'Web App to track your daily emotion.',
+    tech: 'ReactJS',
+    github: 'https://github.com/ranjabi/my-emotions',
+    demo: '',
+  },
+  {
     title: 'DNA Tester',
-    img: 'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg',
-    desc: 'a Full Stack DNA Pattern Matching Website using String Matching and Regular Expression.',
+    img: dnatesterimg,
+    desc: 'DNA Pattern Matching Website using String Matching and Regular Expression.',
     tech: 'ReactJS Node.js PostgreSQL',
     github: 'https://github.com/ranjabi/Tubes3_13520002',
     demo: 'https://dna-tester.netlify.app/',
-  },
-  {
-    title: 'Image Compressor',
-    img: 'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg',
-    desc: 'Image Compression Website using SVD Algorithm in Flask.',
-    tech: 'Flask',
-    github: 'https://github.com/ranjabi/Algeo02-20002',
-    demo: '',
   },
   {
     title: 'ITB Nim Finder',
@@ -26,14 +29,7 @@ const projectList = [
     github: 'https://github.com/ranjabi/itb-nim-finder',
     demo: '',
   },
-  {
-    title: 'My Emotions',
-    img: 'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg',
-    desc: 'Web App to track your daily emotion.',
-    tech: 'ReactJS',
-    github: 'https://github.com/ranjabi/my-emotions',
-    demo: '',
-  },
+
   {
     title: 'To Do List',
     img: 'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg',
@@ -41,23 +37,38 @@ const projectList = [
     tech: 'ReactJS',
     github: 'https://github.com/ranjabi/to-do-list',
     demo: '',
-  }
+  },
 ];
+
+const etc = [
+  {
+    title: 'Image Compressor',
+    img: 'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg',
+    desc: 'Image Compression Website using SVD Algorithm in Flask.',
+    tech: 'Flask',
+    github: 'https://github.com/ranjabi/Algeo02-20002',
+    demo: '',
+  },
+]
 
 const Projects = () => {
   return (
     <div className="container mt-16 md:mt-20 lg:mt-24">
-      <h1 className="mb-12 text-center text-4xl font-semibold">Projects</h1>
-      <div className="px-4 flex flex-wrap justify-center">
-        {projectList.map((project) => {
-          return <Card
-          title={project.title}
-          img={project.img}
-          description={project.desc}
-          tech={project.tech}
-        ></Card>
-        })}
-      </div>
+        <h1 className="mb-12 text-center text-4xl font-semibold">Projects</h1>
+        <div className="px-4 flex flex-wrap justify-center">
+          {projectList.map((project) => {
+            return (
+              <Card
+                key={project.title}
+                title={project.title}
+                img={project.img}
+                github={project.github}
+                description={project.desc}
+                tech={project.tech}
+              ></Card>
+            );
+          })}
+        </div>
     </div>
   );
 };
